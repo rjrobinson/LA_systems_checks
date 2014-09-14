73967547dc407c1e0ff3ae84ab0a1720ad818770
RJ Robinson <robertj.robinson@gmail.com>
2014-09-14 15:42:32 -0400

    added relationships

--- systems_check_5/app/models/list.rb
+++ systems_check_5/app/models/list.rb
@@ -1,2 +1,9 @@
 class List < ActiveRecord::Base
+
+
+  has_many :tasks, dependent: :destroy # thanks to the wonderful documentation at stack overflow.
+
+  has_many :memberships
+  has_many :users, through: :memberships
+
 end
--- systems_check_5/app/models/task.rb
+++ systems_check_5/app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  belongs_to :users
+  belongs_to :lists, through: :membership
 end
